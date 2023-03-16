3ed363692612407135e031c44a6bda47bba47c75
Tom Cariello <tomcariello@gmail.com>
2023-03-16 11:52:04 -0400

    Organized & renamed some files.

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,4 @@
-import axios from "axios";
-import qs from "qs";
-import {client_id, client_secret} from "./keys.js";
-
-async function getToken() {
-  const instance = axios.create({
-    baseURL: "https://services.sentinel-hub.com",
-  });
-
-  const config = {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
-    },
-  };
-
-  const body = qs.stringify({
-    client_id,
-    client_secret,
-    grant_type: "client_credentials",
-  });
-
-  let token;
-
-  // All requests using this instance will have an access token automatically added
-  await instance.post("/oauth/token", body, config).then((resp) => {
-    token = resp.data.access_token;
-  });
-  return token;
-}
+import {getToken} from "./getToken.js";
 
 const token = await getToken();
 
